eb2afa5028374fd2c35b4fcc9a785d5b511a6975
Fumihiro_Maejima <testerwatson1101@gmail.com>
2019-02-10 19:35:01 +0900

    modify setting.

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware('guest:admin');
     }
 
     /**
